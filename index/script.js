f416be7845b4c3208f15b562743cc51ce6c10d1a
Prosper Uche <107319859+futuristicbro@users.noreply.github.com>
2023-01-09 16:03:37 +0100

    updated localStorage Counter to countApi

--- index/script.js
+++ index/script.js
@@ -12,15 +12,9 @@ $(document).ready(function() {
     });
   });
 
-  $(document).ready(function() {
-    if (typeof(Storage) !== "undefined") {
-      if (localStorage.getItem("visitorCount") === null) {
-        localStorage.setItem("visitorCount", 0);
-      }
-      localStorage.setItem("visitorCount", parseInt(localStorage.getItem("visitorCount")) + 1);
-      $("#visitorCount").text(localStorage.getItem("visitorCount"));
-    } else {
-      $("#visitorCount").text("Sorry, your browser does not support local storage.");
-    }
-  });
+
   
+
+function visitorCount(response) {
+    document.getElementById('visits').innerText = response.value;
+}
