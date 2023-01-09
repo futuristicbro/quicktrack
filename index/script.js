222614507fa1c3a84bdfdc6b527ea83f38aeac2c
Prosper Uche <107319859+futuristicbro@users.noreply.github.com>
2023-01-09 22:33:44 +0100

    updated

--- index/script.js
+++ index/script.js
@@ -1,9 +1,4 @@
-var typed = new Typed(".typing", {
-    strings: ["Phone Number", "IMEI Code"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+
 
 $(document).ready(function() {
     $("#warning").hide();
